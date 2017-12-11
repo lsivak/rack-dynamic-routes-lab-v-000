c27b086516ce18a161b5196ef3c13b3145697d30
Lara Sanders <larasanders@gmail.com>
2017-12-11 18:25:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,9 +10,12 @@ class Application
     item = @@items.find{|i| i.name == item_name}
     resp.write item.price
 else
+  resp.write "Item not found"
+  resp.status = 400
+end
+else
   resp.write "Route not found"
   resp.status = 404
-end
 resp.finish
 end
 end
