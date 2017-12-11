cfa54aa33f2a2ab38c278479e85aea2e69599ac3
Lara Sanders <larasanders@gmail.com>
2017-12-11 18:26:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,8 @@ class Application
     item_name = req.path.split("/items/").last
     item = @@items.find{|i| i.name == item_name}
     resp.write item.price
-else
+elsif 
+  !item
   resp.write "Item not found"
   resp.status = 400
 end
