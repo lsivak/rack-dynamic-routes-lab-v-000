a011a8d684f910c1e509a089cd8a0a9b7301e710
Lara Sanders <larasanders@gmail.com>
2017-12-11 18:01:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-  if req.path.match (/items/<ITEM NAME)
+  if req.path.match (/items/<ITEM NAME>)
     @@items.each do |name, price|
     resp.write "#{price}\n"
   end
